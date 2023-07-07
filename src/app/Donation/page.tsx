da531930166664b14ea5334dfd4e02941d7b50c8
Igor de Andrade Lopes <96532725+IgorALopes@users.noreply.github.com>
2023-07-06 21:18:03 -0300

    Donation page linking from navbar

--- src/app/Donation/page.tsx
+++ src/app/Donation/page.tsx
@@ -1,10 +1,11 @@
-
+import Link from 'next/link'
 
 export default function Donation() {
     return (
         <>
             <main>
                 <h1>Doações</h1>
+                <Link href="/">voltar</Link>
             </main>
         </>
     )
