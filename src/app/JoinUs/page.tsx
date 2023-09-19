3ae0ffb78f2dc831eaac5e01c6b0748bdf188afa
lenitoarruda <lenitoarruda@hotmail.com>
2023-09-19 06:47:05 -0300

    Melhorando responsividade da tela Join Us

--- src/app/JoinUs/page.tsx
+++ src/app/JoinUs/page.tsx
@@ -1,55 +1,66 @@
 "use client";
-import Grid from '@mui/material/Grid/Grid'
-import React, { createContext } from 'react';
 import Image from 'next/image'
-import imgHands from '../../assets/img_hands.png'
-import '../../styles/tailwind.css';
-import { TextField, Typography } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { TextField, Typography, Grid } from '@mui/material';
 import { SubmitHandler, useForm } from 'react-hook-form';
+import '../../styles/tailwind.css';
+import imgHands from '../../assets/img_hands.svg'
 
 const JoinUs = () => {
+    const [isSmallScreen, setIsSmallScreen] = useState(false)
     const { register, handleSubmit } = useForm<any>();
     const onSubmit: SubmitHandler<any> = async data => {
         console.log(data);
     };
+    const checkScreenSize = () => {
+        setIsSmallScreen(window.innerWidth < 1200);
+    };
+
+    //Checando largura   
+    useEffect(() => {
+        checkScreenSize();
+        window.addEventListener('resize', checkScreenSize);
+        return () => {
+            window.removeEventListener('resize', checkScreenSize);
+        };
+    }, []);
+
     return (
-        <Grid container>
-            <Grid item xs={8} minHeight='80vh' display='flex' alignItems='center' justifyContent='center'>
-                <div className="rounded-full shadow-custom p-4 bg-green-600" style={{ width: '472px', height: '433px' }}>
-                    <div className="relative" style={{ width: '450px', height: '300px' }}>
-                        <Image
-                            src={imgHands}
-                            alt="Logo do Instituto ISM"
-                            className='ml-12'
-                        />
-                    </div>
+        <Grid container padding={5}>
+            <Grid item xs={12} md={12} lg={6} xl={7} maxHeight={isSmallScreen ? '300px' : '700px'} display='flex' alignItems='center' justifyContent='center'>
+                <div className="relative" style={{ width: `${isSmallScreen ? '20vw' : '33vw'}` }}>
+                    <Image
+                        src={imgHands}
+                        alt="Logo do Instituto ISM"
+                        className={isSmallScreen ? 'ml-5' : 'ml-12'}
+                    />
                 </div>
             </Grid>
-            <Grid item xs={4} display='flex' alignItems='center' container>
+            <Grid item md={12} lg={6} xl={5} display='flex' alignItems='center' justifyContent='center' container>
                 <form onSubmit={handleSubmit(onSubmit)}>
-                    <Grid item>
+                    <Grid item xs={12}>
                         <Typography fontSize={32} fontWeight='bold' color='#2369F6' justifyContent='center' display='flex'>
                             Junte-se a Nós
                         </Typography>
                     </Grid>
-                    <Grid item>
+                    <Grid item xs={12}>
                         <Typography fontSize={35} fontWeight='bold' color='#0038AA'>
                             Ajude a ampliar o Instituto
                         </Typography>
                     </Grid>
-                    <Grid item paddingTop={5}>
+                    <Grid item paddingTop={5} xs={12}>
                         <Typography fontSize={17} >
                             Nome
                         </Typography>
                         <TextField fullWidth required {...register("name")} />
                     </Grid>
-                    <Grid item paddingTop={3}>
+                    <Grid item paddingTop={3} xs={12}>
                         <Typography fontSize={17} >
                             Email
                         </Typography>
                         <TextField fullWidth required {...register("email")} />
                     </Grid>
-                    <Grid item paddingTop={3}>
+                    <Grid item paddingTop={3} xs={12}>
                         <Typography fontSize={17} >
                             Diga em poucas palavras, como você poderia ajudar <br />
                             o <b>Instituto Ser Melhor</b>
@@ -61,8 +72,6 @@ const JoinUs = () => {
                             Enviar
                         </button>
                     </Grid>
-
-
                 </form>
             </Grid>
         </Grid>
