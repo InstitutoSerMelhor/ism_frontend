c7f712a2b9b647b39cebf10a0eac3e7ead79eeeb
Abner <abnerferreira84@gmail.com>
2023-09-20 10:25:54 -0300

    feat: base do novo header pronta.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,10 +21,10 @@ export default function RootLayout({
 }) {
     return (
         <html lang="pt-br">
-            <body className={poppins.className}>
+            <body className="bg-gradient-to-t from-[#fbc2eb] to-[#a6c1ee] h-screen">
                 <NavBar />
-                {children /* children está adicionando o arquivo "page.tsx" (que é a Home) desta pasta ao body do site */}
-                <Footer />
+                {/* {children  children está adicionando o arquivo "page.tsx" (que é a Home) desta pasta ao body do site */}
+                {/* <Footer /> */}
             </body>
         </html>
     )
--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,30 +1,71 @@
+'use client'
 import Image from 'next/image'
-import React from 'react'
-import ism_logo from '../../assets/ism_logo.svg'
-import RouterLinks from './RouterLinks'
+import Link from 'next/link';
+import React, { useState } from 'react'
+import { FaBars } from 'react-icons/fa6';
+import { FaXmark } from 'react-icons/fa6';
+// import ism_logo from '../../assets/ism_logo.svg'
+// import RouterLinks from './RouterLinks'
 
-// a qualidade da Logo não está boa, por isso tive que redimensionar ela toda na tag Image.
+
 const NavBar = () => {
+    const [isToggle, setIsToggle] = useState(false);
+
+    const toggleMenu = () => {
+        setIsToggle(!isToggle);
+    }
+
     return (
-        <nav className='bg-blue-950 h-16 '>
-            <ul className='flex flex-row items-center justify-between h-full p-4'>
-                <Image className='mt-9' src={ism_logo} width={180} alt='Logo do Instituto ISM' />
-                <div className='flex flex-row items-center gap-6 '>
-                    <RouterLinks route='/' title='Home' />
-                    <RouterLinks route='/JoinUs' title='Junte-se a nós' />
-                    <RouterLinks route='/About' title='Sobre nós' />
-                    <RouterLinks route='/News' title='Notícias' />
-                    <RouterLinks route='/Contact' title='Contato' />
-                    <RouterLinks route='/SignIn' title='Login' />
-                    <li>
-                        <button className='h-10 w-32 bg-green-600 rounded-lg text-xs text-white font-light hover:bg-green-900 transition-all duration-150'>
-                            Doar Agora
-                        </button>
-                    </li>
+        <header className='bg-white'>
+            <nav className=' flex justify-between items-center w-[92%] mx-auto py-3'>
+                <div>
+                    <h1>LOGO</h1>
                 </div>
-            </ul>
-        </nav>
+                <div className={`md:static duration-500 absolute bg-white md:min-h-fit min-h-[60vh] left-0 ${isToggle ? 'top-[9%]' : 'top-[-100%]'} md:w-auto w-full flex items-center px-5`}>
+                    <ul className='flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8 text-[#0038AA]'>
+                        <li><Link href="">Home</Link></li>
+                        <li><Link href="">Junte-se a Nós</Link></li>
+                        <li><Link href="">Sobre</Link></li>
+                        <li><Link href="">Contato</Link></li>
+                        <li><Link href="">Login</Link></li>
+                    </ul>
+                </div>
+                <div className='flex items-center gap-6'>
+                    <button className='bg-[#F88D12] text-white px-5 py-2 rounded-lg'>Doa Agora</button>
+                    
+                    {
+                        isToggle 
+                        ? <FaXmark onClick={toggleMenu} className='text-3xl cursor-pointer md:hidden' />
+                        : <FaBars onClick={toggleMenu} className='text-3xl cursor-pointer md:hidden' />
+                    }
+                                    
+                </div>
+            </nav>
+        </header>
     )
 }
+// // a qualidade da Logo não está boa, por isso tive que redimensionar ela toda na tag Image.
+// const NavBar = () => {
+//     return (
+//         <nav className='bg-blue-950 h-16 '>
+//             <ul className='flex flex-row items-center justify-between h-full p-4'>
+//                 <Image className='mt-9' src={ism_logo} width={180} alt='Logo do Instituto ISM' />
+//                 <div className='flex flex-row items-center gap-6 '>
+//                     <RouterLinks route='/' title='Home' />
+//                     <RouterLinks route='/JoinUs' title='Junte-se a nós' />
+//                     <RouterLinks route='/About' title='Sobre nós' />
+//                     <RouterLinks route='/News' title='Notícias' />
+//                     <RouterLinks route='/Contact' title='Contato' />
+//                     <RouterLinks route='/SignIn' title='Login' />
+//                     <li>
+//                         <button className='h-10 w-32 bg-green-600 rounded-lg text-xs text-white font-light hover:bg-green-900 transition-all duration-150'>
+//                             Doar Agora
+//                         </button>
+//                     </li>
+//                 </div>
+//             </ul>
+//         </nav>
+//     )
+// }
 
-export default NavBar
+export default NavBar;
