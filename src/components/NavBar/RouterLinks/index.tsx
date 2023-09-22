63b4166c8d3bbc9ae2bdd54c400b46cb3ba5caac
Abner <abnerferreira84@gmail.com>
2023-09-22 11:48:03 -0300

    fix: ajustei a posição do link de login e botão de doar de acordo com a responsividade.

--- src/components/NavBar/RouterLinks/index.tsx
+++ src/components/NavBar/RouterLinks/index.tsx
@@ -4,11 +4,12 @@ import React from 'react'
 type RouterProps = {
     route: string;
     title: string;
+    classStyle?: string;
 }
 
-const RouterLinks = ({ route, title }: RouterProps) => {
+const RouterLinks = ({ route, title, classStyle }: RouterProps) => {
     return (
-        <li className='flex flex-row items-center gap-1'>
+        <li className={`${classStyle} flex flex-row items-center gap-1`}>
             <Link className='text-base font-medium hover:border-b-2 hover:border-[#0038AA]'
                 href={route}>
                 {title}
--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -11,22 +11,23 @@ const NavBar = ({ isTransparent }: { isTransparent: boolean }) => {
 
     return (
         <header className={`${isTransparent ? '' : 'bg-white'}`}>
-            <nav className=' flex justify-between items-center w-[92%] mx-auto md:pt-5 pt-6 pr-1'>
-                <ul className={`${isToggle ? 'top-[9%]' : 'top-[-100%]'} md:static duration-500 absolute md:min-h-fit min-h-[60vh] left-0 md:translate-x-[17.5%] md:w-auto w-full mx-auto flex items-center px-5`}>
+            <nav className=' flex md:justify-between justify-end items-center w-[92%] mx-auto md:pt-5 pt-6 pr-1'>
+                <ul className={`${isToggle ? 'top-[9%]' : 'top-[-100%]'} md:static duration-500 absolute md:min-h-fit min-h-[60vh] left-0 md:translate-x-[18%] md:w-auto w-full mx-auto flex items-center px-5`}>
                     <div className='flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8 text-white'>
                         <RouterLinks route='/' title='Home' />
                         <RouterLinks route='/JoinUs' title='Junte-se a nós' />
                         <RouterLinks route='/About' title='Sobre nós' />
                         <RouterLinks route='/News' title='Notícias' />
-                        <RouterLinks route='/Contact' title='Contato' />              
+                        <RouterLinks route='/Contact' title='Contato' />
+                        <div className='flex md:flex-row flex-col gap-8 md:ml-16'>
+                            <RouterLinks route='/SignIn' title='Login' />   
+                            <button className='bg-[#F88D12] text-base text-white px-8 py-2 rounded-[8px]'>
+                                Doar Agora
+                            </button>  
+                        </div>
                     </div>
                 </ul>
-                <div className='flex items-center gap-6 text-white'>
-                    <RouterLinks route='/SignIn' title='Login' />   
-                    <button className='bg-[#F88D12] text-base text-white px-8 py-2 rounded-[8px]'>
-                        Doar Agora
-                    </button>          
-                </div>            
+
                 {isToggle 
                     ? <FaXmark onClick={toggleMenu} className='text-4xl text-white cursor-pointer md:hidden' />
                     : <FaBars onClick={toggleMenu} className='text-4xl text-white cursor-pointer md:hidden' />}
