9553122a87aa671fb7fd68abaf7b0c18ce1365da
lenitoarruda <lenitoarruda@hotmail.com>
2023-09-25 06:10:47 -0300

    fix: definindo padding e margin 0 para o main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 import Link from 'next/link'
 
 export default function Home() {
-  return (
-      <>
-          <nav className="bg-red-500">
+    return (
+        <>
+            <nav className="bg-red-500">
                 <Link href="/Donation">Doações</Link>
             </nav>
-            <main>
+            <main className="m-0 p-0" style={{ height: "75vh" }}>
                 <h1>HOME</h1>
-          </main>
-      </>
-  )
+            </main>
+        </>
+    )
 }
